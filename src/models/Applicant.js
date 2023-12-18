c1f99677edf577eb819dcc664e8efa22ca317863
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-12-19 00:33:43 +0100

    Adding application status

--- src/models/Applicant.js
+++ src/models/Applicant.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 import ApplicantStatus from "../utils/ApplicantStatus";
+import ApplicantionStatus from "../utils/ApplicationStatus";
 
 const ApplicantSchema = new mongoose.Schema({
     firstname: String,
@@ -8,7 +9,9 @@ const ApplicantSchema = new mongoose.Schema({
     resume: String,
     coverLetter: String,
     score: {type: Number, default: 0}
-    status: { type: String, enum: Object.values(ApplicantStatus)}
+    applicantStatus: { type: String, enum: Object.values(ApplicantStatus)},
+    applicationStatus: {type: String, enum: Object.values(ApplicantionStatus), default: ApplicantionStatus.NotChecked}
+
 })
 
 const Applicant = mongoose.model('Applicant', ApplicantSchema);
